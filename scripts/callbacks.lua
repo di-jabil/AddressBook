b8680b4739d8ea8f55091100bdf3701910aa2691
di-jabil <di_wang1@jabil.com>
2017-05-26 14:57:47 -0400

    3rd test - d

--- scripts/callbacks.lua
+++ scripts/callbacks.lua
@@ -14,7 +14,7 @@ local gIndex = 0
 function CBLoadList(mapargs) 
 	local data = {}
 	
-  -- this is another test!
+  -- this is another test! again
 	
 	table.sort(address_book, 
 			function(e1, e2)
